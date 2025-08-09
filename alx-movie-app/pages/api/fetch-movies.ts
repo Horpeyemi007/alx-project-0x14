e2120dae1b402b232fd75b9d2cf151f173a1625c
Opeyemi <Opeyemi5055@yahoo.com>
2025-08-09 01:21:25 +0100

    task3

---
+++ alx-movie-app/pages/api/fetch-movies.ts
@@ -0,0 +1,37 @@
+import { MovieProps } from "@/interfaces";
+import { NextApiRequest, NextApiResponse } from "next";
+import { headers } from "next/headers";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  if (request.method === "POST") {
+    const { year, page, genre } = request.body;
+    const date = new Date();
+    const resp = await fetch(
+      `https://moviesdatabase.p.rapidapi.com/titles?year=${
+        year || date.getFullYear()
+      }&sort=year.decr&limit=12&page=${page}&${genre && `genre=${genre}`}`,
+      {
+        headers: {
+          "x-rapidapi-host": "moviesdatabase.p.rapidapi.com",
+          "x-rapidapi-key": `${process.env.MOVIE_API_KEY}`,
+        },
+      }
+    );
+    // console.log("Error in fetching movies is: ", resp);
+
+    if (!resp.ok) throw new Error("Failed to fetch movies");
+
+    const moviesResponse = await resp.json();
+    const movies: MovieProps[] = moviesResponse.results;
+
+    return response.status(200).json({
+      movies,
+    });
+  } else {
+    response.setHeader("Allow", ["POST"]);
+    response.status(405).end(`Method ${request.method} Not Allowed in here`);
+  }
+}
